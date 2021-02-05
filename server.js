7cbfc2ad4041d2dfb4544c0fe894118b6f7f3918
Julie Altman <jtskolnik@gmail.com>
2021-02-04 22:08:12 -0500

    building 5

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(_dirname, "client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 
